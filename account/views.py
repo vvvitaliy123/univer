b6093f3a16045444f2a540298344bbf6de6d4ca3
Vitaliy Zagorskiy <vlodik2282@gmail.com>
2022-11-16 20:29:00 +0200

    Прототип коду реєстрації

--- account/views.py
+++ account/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate 
+from hashlib import md5
 
 
 def reg(request):
     if request.method == 'GET':
         return render(request, 'account/reg.html')
     elif request.method =='POST':
-        # . . .
-        return render(request, 'account/reg.html')
+        # 1. Зчитуємо із форми реєстрації дані:
+        login = request.POST.get('login')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        email = request.POST.get('email')
+
+        # 2. Сценарій реєстрації
+        report = dict()
+        passw = md5(pass1.encode('utf-8')).hexdigest()
+        new_user = User.objects.create_user(login, email, passw)
+        if new_user is None:
+            report['mess'] = 'У реєстрації відмовлено!'
+        else:
+            report['mess'] = 'Ви успішно зареєстровані!'
+
+        # 3. Завантажуємо звіт на сторінку результатів
+        return render(request, 'account/reg_res.html', content=report)
 
 def confirm(request):
     return render(request, 'account/confirm.html')
